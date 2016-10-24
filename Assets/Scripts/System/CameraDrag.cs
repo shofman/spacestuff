67a7bb31bbc37e38ec70ad0b6b02901f15bfbe9b
shofman <stumpitron@gmail.com>
2016-10-23 21:33:05 -0600

    Added side-scroll to the side of the camera (in order to move camera while moving ships around)

--- Assets/Scripts/System/CameraDrag.cs
+++ Assets/Scripts/System/CameraDrag.cs
@@ -10,6 +10,15 @@ public class CameraDrag : MonoBehaviour {
     const int zoomSizeMin = 13;
     const int zoomSizeMax = 100;
 
+    public int boundary = 50; // distance from edge scrolling starts
+    private int screenWidth;
+    private int screenHeight;
+
+    void Start() {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
+
     void Update() {
         if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
             dragOrigin = Input.mousePosition;
@@ -17,20 +26,45 @@ public class CameraDrag : MonoBehaviour {
         }
 
         if (Input.GetMouseButton(0)) {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 pos = getCameraRelativePos(dragOrigin);
             Vector3 move = new Vector3(pos.x * dragSpeedXY, pos.y * dragSpeedXY, 0);
 
             transform.Translate(move, Space.World);
             return;
         }
 
-
-         if (Input.GetAxis("Mouse ScrollWheel") < 0) { // forward
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) { // forward
             Camera.main.fieldOfView = Camera.main.fieldOfView + 5;
-         }
-         if (Input.GetAxis("Mouse ScrollWheel") > 0) {// back
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) {// back
             Camera.main.fieldOfView = Camera.main.fieldOfView - 5;
-         }
-         Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, zoomSizeMin, zoomSizeMax );
+        }
+        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, zoomSizeMin, zoomSizeMax );
+        
+        // Shift the camera if the mouse is within 'boundary' distance of the corners
+        if (Input.mousePosition.x > screenWidth - boundary || Input.mousePosition.x < 0 + boundary) {
+            Vector3 pos = getCameraRelativePos(getCenterOfCamera());
+            Vector3 move = new Vector3(pos.x * dragSpeedXY, 0, 0);
+            transform.Translate(move, Space.World);
+        }
+        if (Input.mousePosition.y > screenHeight - boundary || Input.mousePosition.y < 0 + boundary) {
+            Vector3 pos = getCameraRelativePos(getCenterOfCamera());
+            Vector3 move = new Vector3(0, pos.y * dragSpeedXY, 0);
+            transform.Translate(move, Space.World);
+        }
+    }
+
+    /**
+     * Returns the camera position relative to a specific point (usually center of the screen, or the start of a drag)
+     */
+    private Vector3 getCameraRelativePos(Vector3 relativeTo) {
+        return Camera.main.ScreenToViewportPoint(Input.mousePosition - relativeTo);
+    }
+
+    /**
+     * Returns a Vector3 describing the center of the camera
+     */
+    private Vector3 getCenterOfCamera() {
+        return new Vector3(Screen.width / 2, Screen.height / 2, 0);
     }
 }
