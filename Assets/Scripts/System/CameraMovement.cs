08ad838ff5d29a90966bc19f145edd24632c1e17
shofman <stumpitron@gmail.com>
2017-06-28 20:41:23 -0600

    Added scene to use as a basis for space combat. Added new project settings to allow for flight controls (Roll, Pitch, Yaw). Added Scenes folders, and moved all scenes into area. Simplified controls - changed mouse movement into arrow key movement

--- Assets/Scripts/System/CameraMovement.cs
+++ Assets/Scripts/System/CameraMovement.cs
@@ -23,26 +23,8 @@ public class CameraMovement : MonoBehaviour {
     }
 
     void Update() {
-        // Prevent the scrolling when the mouse has left
-        if (!isMouseWithinScreenBoundaries()) {
-            return;
-        }
-
         if (Input.GetKeyDown("space")) {
             Camera.main.transform.position = startingPos;
-        }
-
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
-            dragOrigin = Input.mousePosition;
-            return;
-        }
-
-        if (Input.GetMouseButton(0)) {
-            Vector3 pos = getCameraRelativePos(dragOrigin);
-            Vector3 move = new Vector3(pos.x * dragSpeedXY, pos.y * dragSpeedXY, 0);
-
-            transform.Translate(move, Space.World);
-            return;
         }
 
         if (Input.GetAxis("Mouse ScrollWheel") < 0) { // forward
@@ -56,55 +38,10 @@ public class CameraMovement : MonoBehaviour {
         // Top speed should be 4.3, lowest speed should be .7. Solving simultaneous equations - 13x + y = .7 and 130x + y = 4.3 gives 2/65 and 3/10
         float cameraZoomSpeedAdjustment = (Camera.main.fieldOfView * (2.0f / 65.0f)) + (3.0f/10.0f);
 
-        // Shift the camera if the mouse is within 'boundary' distance of the corners
-        if (isMouseWithinXBoundary()) {
-            Vector3 pos = getCameraRelativePos(getCenterOfCamera());
-            Vector3 move = new Vector3(pos.x * dragSpeedXY * cameraZoomSpeedAdjustment, 0, 0);
-            if (isMouseWithinScreenBoundaries()) {
-                transform.Translate(move, Space.World);
-            }
-        }
-        if (isMouseWithinYBoundary()) {
-            Vector3 pos = getCameraRelativePos(getCenterOfCamera());
-            Vector3 move = new Vector3(0, pos.y * dragSpeedXY * cameraZoomSpeedAdjustment, 0);
-            if (isMouseWithinScreenBoundaries()) {
-                transform.Translate(move, Space.World);
-            }
-        }
-    }
 
-    /**
-     * Whether the mouse is within the x boundary along the left and right sides of the screen
-     */
-    private bool isMouseWithinXBoundary() {
-        return Input.mousePosition.x > screenWidth - boundary || Input.mousePosition.x < 0 + boundary;
-    }
-
-    /**
-     * Whether the mouse is within the y boundary along the top and bottom sides of the screen
-     */
-    private bool isMouseWithinYBoundary() {
-        return Input.mousePosition.y > screenHeight - boundary || Input.mousePosition.y < 0 + boundary;
-    }
-
-    /**
-     * Returns the camera position relative to a specific point (usually center of the screen, or the start of a drag)
-     */
-    private Vector3 getCameraRelativePos(Vector3 relativeTo) {
-        return Camera.main.ScreenToViewportPoint(Input.mousePosition - relativeTo);
-    }
-
-    /**
-     * Returns a Vector3 describing the center of the camera
-     */
-    private Vector3 getCenterOfCamera() {
-        return new Vector3(Screen.width / 2, Screen.height / 2, 0);
-    }
-
-    /**
-     * Determines whether the mouse is within the screen
-     */
-    private bool isMouseWithinScreenBoundaries() {
-        return (Input.mousePosition.y > 0 && Input.mousePosition.y < Screen.height && Input.mousePosition.x > 0 && Input.mousePosition.x < Screen.width);
+        Vector3 moveHorizonal = new Vector3(Input.GetAxis("Horizontal") * dragSpeedXY * cameraZoomSpeedAdjustment, 0, 0);
+        transform.Translate(moveHorizonal, Space.World);
+        Vector3 moveVertical = new Vector3(0, Input.GetAxis("Vertical") * dragSpeedXY * cameraZoomSpeedAdjustment, 0);
+        transform.Translate(moveVertical, Space.World);
     }
 }
